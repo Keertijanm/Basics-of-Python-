c9f2ba18c2adc21dd6aff84559ec4c9f230561cd
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2021-06-13 22:33:21 +0530

    find exponent of a number
    program to find exponent or power of a number

---
+++ py_exp6.py
@@ -0,0 +1,16 @@
+""" KEERTI GUPTA ,                               1900320130082 - IT-B                    
+                PYTHON -LAB_EXPERIMENT -6
+
+Program -  To write a python program find exponation (Power of a number)
+
+"""
+num = int(input("Enter the number of which you have to find power: "))
+pw = int(input("Enter the power: "))
+
+x = 1
+for n in range(pw):
+    x = x*num
+print("output")
+print(x)
+
+
